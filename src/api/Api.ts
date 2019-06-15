d828360fbba8709ac18a867248fafbbb1c702d36
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2019-06-15 11:07:16 +0200

    Fix linter errors

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -3,16 +3,16 @@ import ky, { Options } from 'ky';
 import { PackageResponseData, SuggestionsResponseData } from './ApiTypes';
 
 export default class API {
-    static async get<Response>(url: string, options?: Options) {
+    public static async get<Response>(url: string, options?: Options) {
         const response = await ky.get(url, options).json<Response>();
         return response;
     }
 
-    static getPackageDetails(packageName: string) {
+    public static getPackageDetails(packageName: string) {
         return API.get<PackageResponseData>(`https://api.npms.io/v2/package/${packageName}`);
     }
 
-    static getSuggestions(query: string) {
+    public static getSuggestions(query: string) {
         return API.get<SuggestionsResponseData[]>(`https://api.npms.io/v2/search/suggestions`, {
             searchParams: {
                 q: query,
