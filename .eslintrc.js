070857efba210dc8a7a6f02b610ed81d1939d0b5
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2019-12-17 23:08:02 +0100

    Configure eslint with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react"
-import { Helmet } from "react-helmet"
-import "../styles/index.css"
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import '../styles/index.css';
 
 function Index() {
-  const [date, setDate] = useState(null)
+  const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch("/api/date")
-      const newDate = await res.text()
-      setDate(newDate)
+      const res = await fetch('/api/date');
+      const newDate = await res.text();
+      setDate(newDate);
     }
-    getDate()
-  }, [])
+    getDate();
+  }, []);
   return (
     <main>
       <Helmet>
@@ -19,12 +19,8 @@ function Index() {
       </Helmet>
       <h1>Gatsby + Node.js (TypeScript) API</h1>
       <h2>
-        Deployed with{" "}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
+        Deployed with{' '}
+        <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
           ZEIT Now
         </a>
         !
@@ -36,11 +32,10 @@ function Index() {
           rel="noreferrer noopener"
         >
           This project
-        </a>{" "}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{" "}
-        which contains a serverless{" "}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{" "}
+        </a>{' '}
+        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two directories,{' '}
+        <code>/src</code> for static content and <code>/api</code> which contains a serverless{' '}
+        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
         <a href="/api/date">
           <code>api/date</code> for the Date API with Node.js (TypeScript)
         </a>
@@ -48,9 +43,9 @@ function Index() {
       </p>
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : "Loading date..."}</p>
+      <p>{date ? date : 'Loading date...'}</p>
     </main>
-  )
+  );
 }
 
-export default Index
+export default Index;
