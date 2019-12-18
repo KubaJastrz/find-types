6af575f24094a914b96cc5322e84114205529644
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2019-12-18 22:04:03 +0100

    Add modern-normalize

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
+import 'modern-normalize';
+
 import Header from './header';
 import './layout.css';
 
