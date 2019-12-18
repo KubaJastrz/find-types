9fb6c6a531156e52b01c31561601754c8449642f
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2019-12-18 20:33:58 +0100

    Add @hot-loader/react-dom

---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage.startsWith('develop')) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          'react-dom': '@hot-loader/react-dom',
+        },
+      },
+    });
+  }
+};
