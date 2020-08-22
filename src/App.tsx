3fc4205e5ad9c8103712d379891d6bb39c798206
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2020-08-22 20:24:57 +0200

    feat: details cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {ReactQueryConfigProvider} from 'react-query'
+import {ReactQueryConfigProvider, ReactQueryProviderConfig} from 'react-query'
 import RQD from 'react-query-devtools'
 
 import {Home} from '/@/pages/Home'
 import {NotFound} from '/@/pages/NotFound'
 
+const queryConfig: ReactQueryProviderConfig = {
+  queries: {
+    retry: 1,
+    staleTime: 10 * 60 * 1000, // 10 minutes
+  },
+}
+
 export const App: React.FC = () => {
   return (
-    <ReactQueryConfigProvider
-      config={{
-        queries: {
-          retry: 1,
-        },
-      }}
-    >
+    <ReactQueryConfigProvider config={queryConfig}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
