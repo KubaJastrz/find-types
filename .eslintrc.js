5c5e300ab00a4dd328381f39ea52fb9419244f6c
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2022-08-21 22:49:37 +0200

    chore: lint2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+  },
 };
