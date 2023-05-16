7d650d2d0429dd7f625feef3454459aac634f264
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2023-05-16 11:43:56 +0200

    test: e2e (#220)

---
+++ tests/test.ts
@@ -0,0 +1,11 @@
+import { test as baseTest } from '@playwright/test';
+import type { MockServiceWorker } from 'playwright-msw';
+import { createWorkerFixture } from 'playwright-msw';
+
+export const test = baseTest.extend<{
+  worker: MockServiceWorker;
+}>({
+  worker: createWorkerFixture(),
+});
+
+export { expect } from '@playwright/test';
