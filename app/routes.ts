72b123da2aff3b9775d8ef8521e22af70187dfaa
Kuba Jastrzębski <jakub.jastrzebski@pm.me>
2026-08-02 18:30:48 +0200

    feat: add /health route

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { index, type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
 	index("routes/index.tsx"),
+	route("health", "routes/health.tsx"),
 
 	route("package", "routes/package.index.tsx"),
 	route("package/:slug", "routes/package.$.tsx"),
